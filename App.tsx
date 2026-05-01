ebcd101c11d9600f47bf5bc194ba1126706f2052
KG-97 <kalevala97@outlook.com>
2026-05-01 13:10:55 +0800

    Fix TS compilation bugs

--- App.tsx
+++ App.tsx
@@ -1,7 +1,12 @@
 import React, { useEffect, useMemo, useState } from 'react';
-import { Brain, Menu, X } from 'lucide-react';
-import { AtlasRoute } from './types';
+import { Activity, Zap, Brain, GraduationCap, Menu, X, House, Download } from 'lucide-react';
+import { AtlasRoute, Tab } from './types';
 import { atlasDefaultRoute, atlasRouteRegistry } from './app/atlasRoutes';
+import StudyHub from './components/StudyHub';
+import NeuronLab from './components/NeuronLab';
+import ActionPotentialLab from './components/ActionPotentialLab';
+import BrainAtlas from './components/BrainAtlas';
+import AITutor from './components/AITutor';
 
 interface BeforeInstallPromptEvent extends Event {
   prompt: () => Promise<void>;
